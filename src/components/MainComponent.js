904bfeabff4f6d16f55ac1ac8b2627ed36bb3624
Blake <blakebonasera@gmail.com>
2020-04-03 15:35:20 -0700

    Intro To Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,13 +6,12 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
 import About from './AboutComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
